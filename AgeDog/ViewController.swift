a3f6c6b568ba0d0f8126b2f8265424cd8db20734
Bruno Santos <bruno.santos@g.globo>
2024-11-16 11:03:02 -0300

    Initial Commit

---
+++ AgeDog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AgeDog
+//
+//  Created by Bruno Santos on 16/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
